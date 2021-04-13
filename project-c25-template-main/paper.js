648bf6c52a0b8b70ebae23c5662a4227ae59e7a3
tejasanime23 <77193959+tejasanime23@users.noreply.github.com>
2021-04-13 18:25:22 +0530

    Add files via upload

---
+++ project-c25-template-main/paper.js
@@ -0,0 +1,39 @@
+class paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+            restitution:0.3,
+            friction:0,
+            density:1.2	
+			}
+            this.image = loadImage("sprites/paper.png");
+			this.x=x;
+			this.y=y;
+			this.r=r;
+            this.body= Bodies.circle(this.x,this.y,(this.r-20)/2,options);
+			World.add(world,this.body);
+            
+	}
+	display()
+	{
+			
+			var  paperpos =this.body.position;	
+			
+
+			function keyPressed() {
+				if (keyCode === UP_ARROW) {
+
+					Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:-145});
+				}
+			}
+
+			push()
+			translate(paperpos.x, paperpos.y);
+			ellipse(0,0,this.r,this.r);
+			pop()
+			
+	}
+
+}
